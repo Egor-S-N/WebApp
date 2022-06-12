87e6032d9f635218dee5af36a3837d209392206c
Egor-N-S <enifakin@list.ru>
2022-06-13 01:54:57 +0300

    Добавлена новая таблица и контроллер для взаимодействия с ней

---
+++ Models/UsersInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebProject.Models
+{
+    public class UsersInfo
+    {
+        public UsersInfo(int id, string lastname, DateTime dob, int age)
+        {
+            InfoID = id;
+            LastName = lastname;
+            Date = dob;
+            Age = age;
+        }
+
+        public int InfoID{get;set;}
+        public string LastName { get; set; }
+        public DateTime Date { get; set; }
+        public int Age { get; set; }
+
+    }
+}
